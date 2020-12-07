1e4cfd5350f94158aa52756707dde55ce5b4a565
meggs1 <megan.schertz@gmail.com>
2020-12-06 22:14:22 -0600

    refactoring cli

--- lib/whats_happening/cli.rb
+++ lib/whats_happening/cli.rb
@@ -2,44 +2,48 @@ class WhatsHappening::CLI
 
     def call
         puts "Welcome to What's Happening!"
-        @input = ""
-        while @input != "exit"
-            get_months
-            list_months
-            get_user_month
-            what_next
-        end
-        goodbye
+        menu
     end
-
-    # def menu
-    #     puts "Enter the number of the month you're interested in:"
-    #     list_months
-    #     get_user_month
-    #     show_celebrations(user_month)
-    # end
-
 
     def get_months
         @months = WhatsHappening::Month.all
     end
 
     def list_months
-        puts "Choose a month"
         @months.each.with_index(1)  {|month, i| puts "#{i}. #{month.name}"}
     end
 
-    def get_user_month
-        user_month = gets.strip.to_i
-        show_celebrations(user_month) if valid_input(user_month, @months)
+    def menu
+        input = nil
+        puts "Enter the number of the month you're interested in:"
+        get_months
+        list_months
+        input = gets.strip.to_i
+        if input > 0 && input < @months.length + 1
+            show_celebrations(input)
+        elsif input == "exit"
+            goodbye
+        else 
+            invalid_entry
+        end
+
+        
     end
 
-    def valid_input(input, data)
-        input.to_i <= data.length && input.to_i > 0
-    end
 
-    def show_celebrations(user_month)
-        month = @months[user_month - 1]
+
+
+    # def get_user_month
+    #     user_month = gets.strip.to_i
+    #     show_celebrations(user_month) if valid_input(user_month, @months)
+    # end
+
+    # def valid_input(user_month, data)
+    #     input.to_i <= data.length && input.to_i > 0
+    # end
+
+    def show_celebrations(input)
+        month = @months[input - 1]
         month.get_celebrations
         puts "Here is what's happening in #{month.name}"
         month.celebrations.each.with_index(1) do |celebration, i|
@@ -64,6 +68,11 @@ class WhatsHappening::CLI
 
     def show_activity(celebration)
         celebration.activity.each {|activity| puts "#{activity}."}
+    end
+
+    def invalid_entry
+       puts  "Sorry, not sure what you mean, try again"
+       menu
     end
 
     def what_next
