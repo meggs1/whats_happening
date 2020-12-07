573f085bf2f31c111d5072c0744109d7801458a8
meggs1 <megan.schertz@gmail.com>
2020-12-06 19:31:51 -0600

    Initial commit

---
+++ lib/whats_happening.rb
@@ -0,0 +1,20 @@
+
+require 'pry'
+require 'nokogiri'
+require 'open-uri'
+require 'httparty'
+
+require_relative './whats_happening/version'
+require_relative './whats_happening/cli'
+require_relative './whats_happening/scraper'
+require_relative './whats_happening/celebration'
+require_relative './whats_happening/month'
+require_relative './whats_happening'
+
+
+
+
+module WhatsHappening
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/whats_happening/version.rb
@@ -0,0 +1,3 @@
+module WhatsHappening
+  VERSION = "0.1.0"
+end
